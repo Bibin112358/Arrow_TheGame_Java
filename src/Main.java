6aa3a45bb03b7cc794d32ac2b10b81bf315f076d
e209940 <e209940@EWOWGY2010-S24.edu.sbl.ch>
2017-01-04 18:10:11 +0100

    first joel commit

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
 	//first school change comment
-	//halloooooooooooooooooooooooooooooooooo
+	//halloooooooooooooooooooooooooooooooooooo
 	public static void main(String[] args) {
 		launch(args);
 	}
